69aa123e082b6aebfa29c8c11d12df1bee177f86
Toby Darmawan <theshadowmaster1210@gmail.com>
2019-12-23 03:38:23 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Toby Darmawan on 12/23/19.
+//  Copyright © 2019 Toby Darmawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
